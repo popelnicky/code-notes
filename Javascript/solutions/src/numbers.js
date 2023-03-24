bba67c97375c786cfd4388c84bd22a1217fade36
Dmitriy Popelnitskiy <popelnicky@live.ru>
2023-03-24 21:38:19 +0300

    [chg] Set up environment for Javascript

--- Javascript/solutions/src/numbers.js
+++ Javascript/solutions/src/numbers.js
@@ -1,9 +1,38 @@
-export default class Numbers {
-    reverse(x) {
-        return 0;
-    }
+export class Numbers {
+  /*
+   * Given 1) a number and 2) exponent. Needs to exponentiate number to exponent
+   *
+   * Examples:
+   *
+   * Input => 2, 3
+   * Output => 8
+   *
+   * Input => -10, 5
+   * Output => -100000
+   *
+   * Constraints:
+   * - An exponentiated number should be in -2^31 to 2^31 - 1 range. When it is out of range - return 0.
+   * - An exponent >= 0. Otherwise return 0.
+   */
+  exponentiate(num, exponent) {
+    return 0;
+  }
 
-    exponentiate(num, exponent) {
-        return 0;
-    }
+  /*
+   * Given an integer number which needs to reverse
+   *
+   * Examples:
+   *
+   * Input => 123
+   * Output => 321
+   *
+   * Input => -120
+   * Output => -21
+   *
+   * Constraints:
+   * A reversed number should be in -2^31 to 2^31 - 1 range. When it is out of range - return 0.
+   */
+  reverse(num) {
+    return 0;
+  }
 }
---
+++ Javascript/solutions/tests/reverse.tests.js
@@ -0,0 +1,75 @@
+import { expect } from "chai";
+import { Numbers } from "../src/numbers.js";
+
+describe("reverseTests", () => {
+  const solutions = new Numbers();
+
+  it("reverseSimpleNumTest", () => {
+    const actual = solutions.reverse(123);
+
+    expect(actual).to.equal(321);
+  });
+
+  it("reverseSimpleNegativeNumTest", () => {
+    const actual = solutions.reverse(-123);
+
+    expect(actual).to.equal(-321);
+  });
+
+  it("reverseNegativePalindromeNumTest", () => {
+    const expected = -333;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("reversePalindromeNumTest", () => {
+    const expected = 999;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("reverseDecNumTest", () => {
+    const actual = solutions.reverse(60);
+
+    expect(actual).to.equal(6);
+  });
+
+  it("reverseOneDigitNumTest", () => {
+    const expected = 8;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("reverseNegativeOneDigitNumTest", () => {
+    const expected = -5;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("reverseZeroTest", () => {
+    const expected = 0;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+
+  // TODO:
+  it("reverseMaxIntNumTest", () => {
+    const expected = 65335;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+
+  //TODO:
+  it("reverseMinIntNumTest", () => {
+    const expected = -65336;
+    const actual = solutions.reverse(expected);
+
+    expect(actual).to.equal(expected);
+  });
+});
